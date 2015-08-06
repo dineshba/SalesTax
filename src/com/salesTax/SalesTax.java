66be9af74e098c6faa45434b9c95e2807dd45336
dineshba <dineshb@thoughtworks.com>
2015-08-06 10:45:45 +0530

    003 | Checking for word IMPORTED in given input irrespective of its position

--- src/com/salesTax/SalesTax.java
+++ src/com/salesTax/SalesTax.java
@@ -6,6 +6,11 @@ public class SalesTax {
     public boolean calculate(String input) {
         String[] strings = input.split(" ");
         int length = strings.length;
+
+        for (String string : strings) {
+            if (string.equals("imported"))
+                return true;
+        }
         return false;
     }
 }
--- test/com/salesTax/SalesTaxTest.java
+++ test/com/salesTax/SalesTaxTest.java
@@ -2,15 +2,22 @@ package com.salesTax;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 public class SalesTaxTest {
 
     @Test
-    public void shouldReturnTrueWhenThereIsNoImportWordInInput() {
+    public void shouldReturnFalseWhenThereIsNoImportWordInInput() {
         SalesTax salesTax = new SalesTax();
 
         assertFalse(salesTax.calculate(" "));
     }
 
+    @Test
+    public void shouldReturnTrueWhenThereIsNoImportWordInInput() {
+        SalesTax salesTax = new SalesTax();
+
+        assertTrue(salesTax.calculate("1 box of imported cups"));
+    }
 }
